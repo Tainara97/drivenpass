45098695f29daafde137a8b3172a2589b28c4155
Tainara <tainara_rafaely@hotmail.com>
2025-10-08 00:22:35 -0300

    Implementa usuario padrão com recurso de seed

---
+++ prisma/seed.ts
@@ -0,0 +1,28 @@
+import prisma from "../src/database";
+import bcrypt from "bcrypt";
+
+export async function main() {
+
+    const hashedPassword = await bcrypt.hash("demo123", 10);
+
+    await prisma.user.upsert({
+        where: { email: "demo@driven.com.br" },
+        update: {},
+        create: {
+            name: "Demo",
+            email: "demo@driven.com.br",
+            password: hashedPassword,
+        },
+    });
+}
+
+main()
+    .then(async () => {
+        await prisma.$disconnect()
+    })
+    .catch(async (e) => {
+        console.error(e);
+        await prisma.$disconnect();
+        process.exit(1);
+    })
+
